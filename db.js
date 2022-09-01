4cd75c724dc5103bb1360ac45b187369f0afedf9
unknown <isratjahan.emu1998@gmail.com>
2022-09-01 16:09:55 +0600

    tags

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const todos = require("./data/todos");
+const tags = require("./data/tags");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
 
 module.exports = {
   todos,
+  tags,
   videos,
   videos2,
   transactions,
-}
-
+};
